dae5efec676be69588b584583bf5c2d743fa6d96
manishkushare <manishkushare@outlook.com>
2021-06-18 10:16:27 +0530

    done REPL and Script processor mode

---
+++ block-BNaaad/script.js
@@ -0,0 +1,10 @@
+console.log("hey");
+function sumOfNum(){
+    let sum = 0;
+    for(let  i = 1; i<= 10; i++){
+        sum += i;
+   
+    }
+    return `sum of 1 to 10 is : ${sum}`;
+}
+console.log(sumOfNum());

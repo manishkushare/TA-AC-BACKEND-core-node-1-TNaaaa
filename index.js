588ef0d72d93389f377885adbc6228efb3154f12
manishkushare <manishkushare@outlook.com>
2021-06-23 13:13:54 +0530

    use buffer and fs module also used destructuring

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("welcome to node.js");
-
-var fs = require("fs");
+var fs = require("fs");// traditional way of loading library
+var {readFile,readFileSync} = require("fs");// using destructring, only getting methods in a file which we require
 
 fs.readFile("./block-BNaaag/content.md",(err,content) => {
     setTimeout(() => console.log("Hey, am async code",content),1000);
@@ -9,6 +9,8 @@ fs.readFile("./block-BNaaag/content.md",(err,content) => {
     setTimeout(() => console.log("Hey, am async code",content.toString()),2000);
 });
 
+let result = readFileSync("./block-BNaaag/content.md")
+console.log(result," :result");
 var buff1 = Buffer.alloc(10);
 console.log(buff1);
 buff1.write("Welcome to Buffer");

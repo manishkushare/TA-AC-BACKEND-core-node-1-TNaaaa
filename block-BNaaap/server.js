409474f8c93e36b0e1cd86ee4b42f0db6b95438c
manishkushare <manishkushare@outlook.com>
2021-06-24 23:39:36 +0530

    learning postman and response object

--- block-BNaaap/server.js
+++ block-BNaaap/server.js
@@ -6,7 +6,7 @@ function handleServer(req,res){
     console.log(req.method);
     res.statusCode = 201;
     res.setHeader("Content-Type","text/html");
-    res.end("hurray!!!!!!!");
+    res.end("<h1>Welcome</h1>");
 }
 
 server.listen(4444, () => console.log("welcome to node.js!!!!!!!!!"))
